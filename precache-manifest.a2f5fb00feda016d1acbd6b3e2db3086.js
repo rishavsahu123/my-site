a32fe79dd9b2d56815a438be367cb585ad28ee0d
rishavsahu123 <rishavsahu123@gmail.com>
2020-05-05 19:59:18 +0530

    Updates

---
+++ precache-manifest.a2f5fb00feda016d1acbd6b3e2db3086.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b38406e18a9edc56467614ab5a1cfe8f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "703bffb46f6b01252c8c",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cc5b60f530ec9ef2959d",
+    "url": "/static/js/2.3dd64d21.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3dd64d21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "703bffb46f6b01252c8c",
+    "url": "/static/js/main.d045526a.chunk.js"
+  },
+  {
+    "revision": "415e9bcf6c081ea50565",
+    "url": "/static/js/runtime-main.f8c5b4be.js"
+  }
+]);
